2680436b23da2cc1a7a5eccce18e496d8857930d
Brian Johnson <bjthinks@gmail.com>
2025-04-27 15:11:39 -0700

    Make a list of polynomial tests

--- app/Tests.hs
+++ app/Tests.hs
@@ -9,14 +9,14 @@ testEqual name x y = TestCase $ assertEqual name x y
 testTrue :: String -> Bool -> Test
 testTrue name b = TestCase $ assertBool name b
 
-test1 :: Test
-test1 = testEqual "First test" (1+1) (2 :: Int)
+polynomialTests :: Test
+polynomialTests = TestList [testEqual "First test" (1+1) (2 :: Int)]
 
 test2 :: Test
 test2 = testTrue "Second test" $ 2*2 == (4 :: Int)
 
 tests :: Test
-tests = TestList [test1, test2]
+tests = TestList [polynomialTests, test2]
 
 runTests :: IO Counts
 runTests = runTestTT tests
