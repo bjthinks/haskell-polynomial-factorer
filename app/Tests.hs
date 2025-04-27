89c501b4f03e79466d86b54ed29945f56a0e23f2
Brian Johnson <bjthinks@gmail.com>
2025-04-27 14:50:53 -0700

    Eliminate warnings

--- app/Tests.hs
+++ app/Tests.hs
@@ -2,9 +2,14 @@ module Tests where
 
 import Test.HUnit
 
-test1 = TestCase $ assertEqual "First test" 2 (1+1)
-test2 = TestCase $ assertBool "Second test" $ 2*2 == 4
+test1 :: Test
+test1 = TestCase $ assertEqual "First test" (1+1) (2 :: Int)
 
-tests = TestList [TestLabel "test1" test1, TestLabel "test2" test2]
+test2 :: Test
+test2 = TestCase $ assertBool "Second test" $ 2*2 == (4 :: Int)
 
+tests :: Test
+tests = TestList [test1, test2]
+
+runTests :: IO Counts
 runTests = runTestTT tests
