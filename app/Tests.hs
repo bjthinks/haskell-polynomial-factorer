9fcba3d7a16cd166edb6ccefdb51cbe89a1d3ab5
Brian Johnson <bjthinks@gmail.com>
2025-04-27 15:16:02 -0700

    Use HUnit operators

--- app/Tests.hs
+++ app/Tests.hs
@@ -3,14 +3,13 @@ module Tests where
 import Test.HUnit
 import Polynomial
 
-testEqual :: (Eq a, Show a) => String -> a -> a -> Test
-testEqual name x y = TestCase $ assertEqual name x y
-
 testTrue :: String -> Bool -> Test
 testTrue name b = TestCase $ assertBool name b
 
 polynomialTests :: Test
-polynomialTests = TestList [testEqual "First test" (1+1) (2 :: Int)]
+polynomialTests = TestList [
+  "First test" ~: 1+1 ~=? (2 :: Int)
+  ]
 
 test2 :: Test
 test2 = testTrue "Second test" $ 2*2 == (4 :: Int)
