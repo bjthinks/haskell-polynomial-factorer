ba7ec35ffce308f4efce8c65724c5c8828720173
Brian Johnson <bjthinks@gmail.com>
2025-04-27 15:17:54 -0700

    Simplify Tests

--- app/Tests.hs
+++ app/Tests.hs
@@ -3,19 +3,13 @@ module Tests where
 import Test.HUnit
 import Polynomial
 
-testTrue :: String -> Bool -> Test
-testTrue name b = TestCase $ assertBool name b
-
 polynomialTests :: Test
 polynomialTests = TestList [
   "First test" ~: 1+1 ~=? (2 :: Int)
   ]
 
-test2 :: Test
-test2 = testTrue "Second test" $ 2*2 == (4 :: Int)
-
 tests :: Test
-tests = TestList [polynomialTests, test2]
+tests = TestList [polynomialTests]
 
 runTests :: IO Counts
 runTests = runTestTT tests
