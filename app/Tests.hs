50dbba4437cda81b668866f5d78bae6236acdac9
Brian Johnson <bjthinks@gmail.com>
2025-04-27 15:52:55 -0700

    More tests of Show Polynomial. Seems to work right.

--- app/Tests.hs
+++ app/Tests.hs
@@ -3,8 +3,8 @@ module Tests where
 import Test.HUnit
 import Polynomial
 
-polynomialTests :: Test
-polynomialTests = TestList [
+showPolynomial :: Test
+showPolynomial = TestList [
   "Show 0" ~: "0" ~=? show (Polynomial [(0,0)]),
   "Show 1" ~: "1" ~=? show (Polynomial [(1,0)]),
   "Show 5" ~: "5" ~=? show (Polynomial [(5,0)]),
@@ -19,8 +19,24 @@ polynomialTests = TestList [
   "Show -x^2" ~: "-x^2" ~=? show (Polynomial [(-1,2)]),
   "Show -5x^2" ~: "-5x^2" ~=? show (Polynomial [(-5,2)]),
   "Show -53x^27" ~: "-53x^27" ~=? show (Polynomial [(-53,27)]),
-  "Show x+1" ~: "x+1" ~=? show (Polynomial [(1,1),(1,0)])
-  ]
+  "Show x+1" ~: "x+1" ~=? show (Polynomial [(1,1),(1,0)]),
+  "Show x-1" ~: "x-1" ~=? show (Polynomial [(1,1),(-1,0)]),
+  "Show -x+1" ~: "-x+1" ~=? show (Polynomial [(-1,1),(1,0)]),
+  "Show -x-1" ~: "-x-1" ~=? show (Polynomial [(-1,1),(-1,0)]),
+  "Show 5x+3" ~: "5x+3" ~=? show (Polynomial [(5,1),(3,0)]),
+  "Show 5x-3" ~: "5x-3" ~=? show (Polynomial [(5,1),(-3,0)]),
+  "Show -5x+3" ~: "-5x+3" ~=? show (Polynomial [(-5,1),(3,0)]),
+  "Show -5x-3" ~: "-5x-3" ~=? show (Polynomial [(-5,1),(-3,0)]),
+  "Show x^2+x+1" ~: "x^2+x+1" ~=? show (Polynomial [(1,2),(1,1),(1,0)]),
+  "Show -x^2+x+1" ~: "-x^2+x+1" ~=? show (Polynomial [(-1,2),(1,1),(1,0)]),
+  "Show x^2-x+1" ~: "x^2-x+1" ~=? show (Polynomial [(1,2),(-1,1),(1,0)]),
+  "Show 4x^2+4x+1" ~: "4x^2+4x+1" ~=? show (Polynomial [(4,2),(4,1),(1,0)]),
+  "Show -4x^2-4x-1" ~: "-4x^2-4x-1" ~=?
+    show (Polynomial [(-4,2),(-4,1),(-1,0)]),
+  () ~=? ()]
+
+polynomialTests :: Test
+polynomialTests = TestList [showPolynomial]
 
 tests :: Test
 tests = TestList [polynomialTests]
