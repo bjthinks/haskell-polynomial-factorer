cd43ff33bc4aea36d7dc70b3adf3d84d8c4c645c
Brian Johnson <bjthinks@gmail.com>
2025-04-27 15:05:34 -0700

    Helper functions in Tests

--- app/Tests.hs
+++ app/Tests.hs
@@ -2,11 +2,17 @@ module Tests where
 
 import Test.HUnit
 
+testEqual :: (Eq a, Show a) => String -> a -> a -> Test
+testEqual name x y = TestCase $ assertEqual name x y
+
+testTrue :: String -> Bool -> Test
+testTrue name b = TestCase $ assertBool name b
+
 test1 :: Test
-test1 = TestCase $ assertEqual "First test" (1+1) (2 :: Int)
+test1 = testEqual "First test" (1+1) (2 :: Int)
 
 test2 :: Test
-test2 = TestCase $ assertBool "Second test" $ 2*2 == (4 :: Int)
+test2 = testTrue "Second test" $ 2*2 == (4 :: Int)
 
 tests :: Test
 tests = TestList [test1, test2]
