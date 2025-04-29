6b66e394351ff47abf9c0bee9d650017de0fbdba
Brian Johnson <bjthinks@gmail.com>
2025-04-29 13:28:51 -0700

    Make ModularPolynomial a Num

--- app/ModularPolynomial.hs
+++ app/ModularPolynomial.hs
@@ -21,7 +21,39 @@ makeModularPolynomial m =
     addLikeTerms [] = []
     addLikeTerms [t] = [canonical t]
     addLikeTerms (t1@(c1,e1):t2s@((c2,e2):ts))
-      | e1 == e2 = addLikeTerms (((c1+c2),e1):ts)
+      | e1 == e2 = addLikeTerms (((c1+c2) `mod` m,e1):ts)
       | otherwise = canonical t1 : addLikeTerms t2s
     canonical (c,e) = (c `mod` m,e)
     sortTerms = reverse . sortOn snd
+
+instance Num ModularPolynomial where
+  ModularPolynomial m1 xs + ModularPolynomial m2 ys
+    | m1 /= m2 = error "Incompatible modulus in +"
+    | otherwise = ModularPolynomial m1 $ addTerms xs ys
+    where
+      addTerms ps [] = ps
+      addTerms [] qs = qs
+      addTerms allps@(t1@(c1,e1):ps) allqs@(t2@(c2,e2):qs)
+        | e1 > e2 = t1 : addTerms ps allqs
+        | e1 < e2 = t2 : addTerms allps qs
+        | (c1+c2) `mod` m1 == 0 = addTerms ps qs
+        | otherwise = ((c1+c2) `mod` m1,e1) : addTerms ps qs
+  -- * could be made more efficient
+  -- for instance, join together like terms before collecting all possible
+  -- cross terms into one giant list...
+  ModularPolynomial m1 xs * ModularPolynomial m2 ys
+    | m1 /= m2 = error "Incompatible modulus in *"
+    | otherwise = makeModularPolynomial m1 $ multiplyTerms xs ys
+    where
+      multiplyTerms [] _ = []
+      multiplyTerms (p:ps) qs = multiplyTerm p qs ++ multiplyTerms ps qs
+      multiplyTerm _ [] = []
+      multiplyTerm t@(c1,e1) ((c2,e2):ts) =
+        ((c1*c2) `mod` m1,e1+e2) : multiplyTerm t ts
+  negate (ModularPolynomial m xs) = ModularPolynomial m $ negateTerms xs
+    where
+      negateTerms [] = []
+      negateTerms ((c,e):ts) = ((-c) `mod` m,e) : negateTerms ts
+  abs _ = error "No abs for ModularPolynomial"
+  signum _ = error "No signum for ModularPolynomial"
+  fromInteger _ = error "No fromInteger for ModularPolynomial"
